41ce98cef3e37d0997881c78e42e8f909c0b1c31
Chenlili <986220912@qq.com>
2021-01-26 15:56:51 +0800

    完成一系列提交

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,15 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: $--color-primary;
 }
 
 #nav {
   padding: 30px;
-
+  text-align: center;
   a {
     font-weight: bold;
     color: #2c3e50;
-
     &.router-link-exact-active {
       color: #42b983;
     }
--- src/components/Practice.vue
+++ src/components/Practice.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     Practice
+    <ul>
+      <li>面包屑一级导航</li>
+      <li>二级导航</li>
+      <li>内容</li>
+    </ul>
+<!--    哈哈哈-->
   </div>
 </template>
 
@@ -10,4 +16,28 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+ul {
+  margin: 10px;
+  background: #ccc;
+  padding: 10px;
+  border: 1px solid #999;
+  list-style: none;
+  &::after {
+    clear: both;
+    content: "";
+    display: block;
+  }
+}
+li {
+  float: left;
+  margin-left: 10px;
+  &::after {
+    content: "/";
+    margin-left: 10px;
+  }
+}
+li:last-child::after {
+  content: none;
+}
+</style>
