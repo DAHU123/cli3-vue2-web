0c830f1ae21cde504354e6ca8fb8486bc299bdec
Chenlili <986220912@qq.com>
2021-01-26 14:42:29 +0800

    完成一系列操作

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <el-button type="primary" @click="handleClick">点击</el-button>
+    <!--    <img src="@/assets/logo.png" />-->
+    <!--    <p>我是超长的文字</p>-->
+    <div class="parent">
+      <div class="float">浮动元素</div>
+    </div>
+    <!--    <div style="height: 100px;width: 100px;float: left;background: lightblue;">我是一个左浮动的元素</div>-->
+    <!--    <div style="width: 200px; height: 200px;background: #eee;overflow: hidden">我是一个没有设置浮动,-->
+    <!--      也没有触发 BFC 元素, width: 200px; height:200px; background: #eee;</div>-->
+    <div class="parent-1">
+      <div class="child">child</div>
+    </div>
   </div>
 </template>
 
@@ -28,18 +38,63 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+img {
+  float: left;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+p {
+  overflow: hidden;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.parent {
+  overflow: hidden;
 }
-a {
-  color: #42b983;
+.float {
+  float: left;
+}
+.parent-1 {
+  width: 500px;
+  height: 500px;
+  background: #42b983;
+  /*方式1*/
+  /*display: flex;*/
+  /*justify-content: center;*/
+  /*align-items: center;*/
+  /*定位*/
+  /*position: relative;*/
+  /*栅格布局*/
+  /*display: grid;*/
+  /*方式5*/
+  text-align: center;
+  &::before {
+    content: "";
+    display: inline-block;
+    width: 0;
+    height: 100%;
+    vertical-align: middle;
+  }
+  .child {
+    width: 200px;
+    height: 200px;
+    background: pink;
+    /*display: table-cell;*/
+    /*vertical-align: middle;*/
+    /*定位方式2*/
+    /*position: absolute;*/
+    /*left: 50%;*/
+    /*top: 50%;*/
+    /*<!--transform: translate(-50%, -50%);-->*/
+    /*定位方式3*/
+    /*position: absolute;*/
+    /*left: 0;*/
+    /*top: 0;*/
+    /*right: 0;*/
+    /*bottom: 0;*/
+    /*margin: auto;*/
+    /*方式4*/
+    /*justify-self: center;*/
+    /*align-self: center;*/
+    /*方式5*/
+    display: inline-block;
+    vertical-align: middle;
+  }
 }
 </style>
---
+++ src/components/Practice.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Practice
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Practice"
+};
+</script>
+
+<style scoped></style>
