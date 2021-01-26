53537644979d193145e7c6e5c55d08ec34a9a1fe
Chenlili <986220912@qq.com>
2021-01-26 14:49:57 +0800

    完成一系列路由的配置

--- src/router/routesMap.js
+++ src/router/routesMap.js
@@ -1,6 +1,17 @@
 export const constantRouterMap = [
   {
     path: "/",
+    redirect: {
+      name: "Mine"
+    }
+  },
+  {
+    path: "/",
+    name: "Mine",
+    component: () => import("../views/Mine.vue")
+  },
+  {
+    path: "/home",
     name: "Home",
     component: () => import("../views/Home.vue")
   },
---
+++ src/views/Mine.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Mine
+    <Practice />
+  </div>
+</template>
+
+<script>
+import Practice from "@/components/Practice";
+
+export default {
+  name: "Mine",
+  components: { Practice }
+};
+</script>
+
+<style scoped></style>
